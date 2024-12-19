89225236f310a63429c10c606abb7c37793a1e6c
BotaniheskiySad <Denisov2956151998@yandex.ru>
2024-12-19 14:41:02 +0300

    Update file_o.h
    исправление файла для объявления функций

--- file_o.h
+++ file_o.h
@@ -1,8 +1,6 @@
-#ifndef TEXT_O_H
-#define TEXT_O_H
-
-void replace_text(const char *filename, const char *old_text, const char *new_text);
-void delete_lines(const char *filename, const char *pattern);
-void add_text(const char *filename, const char *text, int is_front);
+#ifndef FILE_O_H
+#define FILE_O_H
+FILE *open_file_read(const char *filename);
+FILE *open_file_write(const char *filename);
 
 #endif
