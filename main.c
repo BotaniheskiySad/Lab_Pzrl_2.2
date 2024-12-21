029efcd2e8a3830ab41fd63089166752d9f981f0
BotaniheskiySad <Denisov2956151998@yandex.ru>
2024-12-21 18:46:26 +0300

    Update main.c
    исправление main файла

--- main.c
+++ main.c
@@ -1,46 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "file_o.h"
 #include "text_o.h"
 
-void print_comands() 
-{  
-  printf("Ошибка!\n");
-  printf("./sed_simplified input.txt -r “old text” “new text”\n"); 
-  printf("./sed_simplified input.txt -d “text to delete”\n");
-  printf("./sed_simplified input.txt -i -f “insert text front”\n");
-  printf("./sed_simplified input.txt -i -b “insert text back”\n");
+void print_comands() {
+    printf("Ошибка!\n");
+    printf("./sed_simplified input.txt -r 'old text' 'new text'\n");
+    printf("./sed_simplified input.txt -d 'text to delete'\n");
+    printf("./sed_simplified input.txt -i -I 'text' (добавить текст в начало файла)\n");
+    printf("./sed_simplified input.txt -i -b 'text' (добавить текст в конец файла)\n");
 }
-int main(argc, char *avrg[])
-{
-  if (atgc < 3)
-  {
-    print_comands();
-    return 1;
-  }
-  char *filename = argv[1];
-if (strcmp(argv[2], "-r") == 0 && argc == 5) {
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        print_comands();
+        return 1;
+    }
+
+    char* filename = argv[1];
+    char* position = NULL;
+
+    if (argc == 5) {
+        position = argv[3];
+    }
+
+    if (strcmp(argv[2], "-r") == 0 && argc == 5) {
         replace_text(filename, argv[3], argv[4]);
     } else if (strcmp(argv[2], "-d") == 0 && argc == 4) {
         delete_lines(filename, argv[3]);
     } else if (strcmp(argv[2], "-i") == 0 && argc == 5) {
-        if (strcmp(argv[3], "-f") == 0) {
-            add_text(filename, argv[4], 1);
-        } else if (strcmp(argv[3], "-b") == 0) {
-            add_text(filename, argv[4], 0);
-        } else {
-            print_usage();
-            return 1;
-        }
+        add_text(filename, argv[4], argv[3]);
     } else {
-        print_usage();
+        print_comands();
         return 1;
     }
 
     return 0;
 }
-  
-}
-
 
