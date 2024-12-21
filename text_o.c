68f853d9533fdfafc740759b28b1171e3394f178
BotaniheskiySad <Denisov2956151998@yandex.ru>
2024-12-21 18:47:23 +0300

    Update text_o.c
    исправление багов

--- text_o.c
+++ text_o.c
@@ -1,34 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "file_o.h"
 #include "text_o.h"
 
-void replace_text(const char *filename, const char *old_text, const char *new_text) {
-    FILE *file = open_file_read(filename);
-    FILE *temp = open_file_write("temp.txt");
+void replace_text(const char* filename, const char* old_text, const char* new_text) {
+    FILE* file = fopen(filename, "r");
+    FILE* temp = fopen("temp.txt", "w");
+
+    if (!file || !temp) {
+        perror("ошибка открытия");
+        exit(EXIT_FAILURE);
+    }
 
     char line[1024];
+    char buffer[1024];
     while (fgets(line, sizeof(line), file)) {
-        char *pos = NULL;
+        char* pos = NULL;
+        buffer[0] = '\0';
+
         while ((pos = strstr(line, old_text))) {
             *pos = '\0';
-            fprintf(temp, "%s%s", line, new_text);
-            strcpy(line, pos + strlen(old_text));
+            strncat(buffer, line, sizeof(buffer) - strlen(buffer) - 1);
+            strncat(buffer, new_text, sizeof(buffer) - strlen(buffer) - 1);
+            strncpy(line, pos + strlen(old_text), sizeof(line) - 1);
         }
-        fprintf(temp, "%s", line);
+
+        strncat(buffer, line, sizeof(buffer) - strlen(buffer) - 1);
+        fprintf(temp, "%s", buffer);
     }
 
     fclose(file);
     fclose(temp);
 
-    rename("temp.txt", filename);
+    if (rename("temp.txt", filename) != 0) {
+        perror("ошибка названия ");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void delete_lines(const char *filename, const char *pattern) 
-{
-    FILE *file = open_file_read(filename);
-    FILE *temp = open_file_write("temp.txt");
+void delete_lines(const char* filename, const char* pattern) {
+    FILE* file = fopen(filename, "r");
+    FILE* temp = fopen("temp.txt", "w");
+
+    if (!file || !temp) {
+        perror("ошибка открытия");
+        exit(EXIT_FAILURE);
+    }
 
     char line[1024];
     while (fgets(line, sizeof(line), file)) {
@@ -40,25 +57,43 @@ void delete_lines(const char *filename, const char *pattern)
     fclose(file);
     fclose(temp);
 
-    rename("temp.txt", filename);
+    if (rename("temp.txt", filename) != 0) {
+        perror("ошибка переназвания файла");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void add_text(const char *filename, const char *text, int is_front) {
-    FILE *file = open_file_read(filename);
-    FILE *temp = open_file_write("temp.txt");
+void add_text(const char* filename, const char* text, const char* position) {
+    FILE* file = fopen(filename, "r");
+    FILE* temp = fopen("temp.txt", "w");
+
+    if (!file || !temp) {
+        perror("Ошибка открытия файла");
+        exit(EXIT_FAILURE);
+    }
 
     char line[1024];
+
+    // Добавить текст в начало
+    if (strcmp(position, "-I") == 0) {
+        fprintf(temp, "%s\n", text);
+    }
+
+    // Обработать каждую строку
     while (fgets(line, sizeof(line), file)) {
-        if (is_front) {
-            fprintf(temp, "%s%s", text, line);
-        } else {
-            line[strcspn(line, "\n")] = '\0'; // Удаляем '\n' в конце строки
-            fprintf(temp, "%s%s\n", line, text);
-        }
+        fprintf(temp, "%s", line);
+    }
+
+    // Добавить текст в конец
+    if (strcmp(position, "-b") == 0) {
+        fprintf(temp, "%s\n", text);
     }
 
     fclose(file);
     fclose(temp);
 
-    rename("temp.txt", filename);
+    if (rename("temp.txt", filename) != 0) {
+        perror("Ошибка переназвания");
+        exit(EXIT_FAILURE);
+    }
 }
