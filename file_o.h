2b8df838577b0bdc56e38764a11e145080857b30
BotaniheskiySad <Denisov2956151998@yandex.ru>
2024-12-21 18:47:48 +0300

    Update file_o.h

--- file_o.h
+++ file_o.h
@@ -1,6 +1,8 @@
-#ifndef FILE_O_H
-#define FILE_O_H
-FILE *open_file_read(const char *filename);
-FILE *open_file_write(const char *filename);
+#ifndef TEXT_O_H
+#define TEXT_O_H
 
+void replace_text(const char *filename, const char *old_text,const char* new_text);
+void delete_lines(const char *filename, const char *pattern);
+void add_text(const char *filename, const char *text, const char *position);
 #endif
+
