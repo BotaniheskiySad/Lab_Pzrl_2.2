71911805f67b4156809e929881ba5fd1ae0094b4
BotaniheskiySad <Denisov2956151998@yandex.ru>
2024-12-21 19:06:11 +0300

    dell file_o.h

--- file_o.h
+++ file_o.h
@@ -1,8 +1,1 @@
-#ifndef TEXT_O_H
-#define TEXT_O_H
-
-void replace_text(const char *filename, const char *old_text,const char* new_text);
-void delete_lines(const char *filename, const char *pattern);
-void add_text(const char *filename, const char *text, const char *position);
-#endif
-
+dell
